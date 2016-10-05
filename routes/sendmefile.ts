eb6b7e4935765e8b041b5cca7f7f23a1f89fe95b
Diego Majuf <diego.majluf@gmail.com>
2016-10-05 08:56:29 -0300

    1

--- routes/sendmefile.ts
+++ routes/sendmefile.ts
@@ -1,17 +1,11 @@
 import * as express  from 'express';
 import * as fs from 'fs';
-var xml4js = require('xml4js');
-var parser: any;
+
 export let router = express.Router();
 
 
 fs.readFile('../schemas/LibrosCV/LibroCV_v10.xsd', { encoding: 'utf-8' }, (err1, data) => {
-    parser = new xml4js.Parser({});
-    parser.addSchema("http://www.sii.cl/SiiDte", data, function (err: any, importsAndIncludes: any) {
-        // importsAndIncludes contains schemas to be added as well to satisfy all imports and includes found in schema.xsd
-        console.log('respuesta')
-        if (err) return console.log(err)
-    });
+
 });
 
 
@@ -20,11 +14,6 @@ router.post('/', function (req, res) {
         req.setEncoding('utf8')
         let str = <string>req.read();
         console.log(str.length);
-        parser.parseString(str, function (err: Error, result: any) {
-            if (err) return console.log(err)
-            console.log(typeof (result));
-            res.send(result)
-        });
 
     });
 });
