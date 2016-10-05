09620c14ea1c3bc9aefaf20bd35c0ce4fa60b18d
Diego Majuf <diego.majluf@gmail.com>
2016-10-05 07:37:29 -0300

    1

--- routes/sendmefile.ts
+++ routes/sendmefile.ts
@@ -5,10 +5,12 @@ var parser: any;
 export let router = express.Router();
 
 
-fs.readFile('../schemas/LibrosCV/LibroCV_v10.xsd', { encoding: 'utf-8' }, (err, data) => {
+fs.readFile('../schemas/LibrosCV/LibroCV_v10.xsd', { encoding: 'utf-8' }, (err1, data) => {
     parser = new xml4js.Parser({});
-    parser.addSchema('http://www.example.com/Schema', data, function (err: any, importsAndIncludes: any) {
-        // importsAndIncludes contains schemas to be added as well to satisfy all imports and includes found in schema.xsd 
+    parser.addSchema("http://www.sii.cl/SiiDte", data, function (err: any, importsAndIncludes: any) {
+        // importsAndIncludes contains schemas to be added as well to satisfy all imports and includes found in schema.xsd
+        console.log('respuesta')
+        if (err) return console.log(err)
     });
 });
 
@@ -18,12 +20,12 @@ router.post('/', function (req, res) {
         req.setEncoding('utf8')
         let str = <string>req.read();
         console.log(str.length);
-        res.send(str)
-        parser.parseString(str, function (err: Error, result:any) {
-            console.log(typeof(result));
+        parser.parseString(str, function (err: Error, result: any) {
+            if (err) return console.log(err)
+            console.log(typeof (result));
             res.send(result)
         });
-        
+
     });
 });
 
