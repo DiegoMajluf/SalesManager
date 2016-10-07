ca3355663a1883d57780e8075338acd90ca3c170
Diego Majuf <diego.majluf@gmail.com>
2016-10-07 08:42:03 -0300

    1

--- routes/sendmefile.ts
+++ routes/sendmefile.ts
@@ -6,20 +6,23 @@ import {DOMParser} from 'xmldom'
 
 export let router = express.Router();
 
-router.post('/', function (req, res) {
+router.post('/', function (req, res, next) {
     req.on('readable', function () {
         req.setEncoding('utf8')
         let str = <string>req.read();
-        // let rd = new DOMParser({
-        //     locator: {},
-        //     errorHandler: {
-        //         warning: (w) => console.warn(w),
-        //         error: (e) => console.error(e),
-        //         fatalError: (e) => console.error(e)
-        //     }
-        // })
-        // let doc = rd.parseFromString(str, "application/xml")
-        res.send(str)
+        let rd = new DOMParser({
+            locator: {},
+            errorHandler: {
+                warning: (w) => console.warn(w),
+                error: (e) => console.error(e),
+                fatalError: (e) => console.error(e)
+            }
+        })
+        let doc = rd.parseFromString(str, "application/xml")
+        let dd = new DTE.SetDTE();
+        dd.ParseFromXMLElement(doc.documentElement)
+        res.write(JSON.stringify(dd))
+        res.send();
     });
         
 });
