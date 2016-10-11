c74f27ff54ce98c3ecae43d5fcb526eda7147c89
Diego Majluf <diego.majluf@gmail.com>
2016-10-11 17:40:33 -0300

    1

--- cliente/script.ts
+++ cliente/script.ts
@@ -1,19 +1,30 @@
 import * as DTE from './dtes'
 
+let pre = <HTMLPreElement>document.getElementById('pre');
+
+function sendText() {
+    let txt = <HTMLTextAreaElement>document.getElementById('textArea');
+
+    let xml = new XMLHttpRequest();
+    xml.open('post', '/sendmefile');
+    xml.onload = () => pre.textContent = xml.response;
+    xml.send(new Blob([txt.value], {type: "application/xml"}))
+}
+
+
 (() => {
 
 
 
     let inp = <HTMLInputElement>document.getElementById('inpFile');
+    let but = <HTMLInputElement>document.getElementById('button');
+     but.addEventListener('click', sendText);
 
     inp.addEventListener('change', () => {
 
         let xml = new XMLHttpRequest();
         xml.open('post', '/sendmefile');
-        xml.onload = function () {
-            let pre = <HTMLPreElement>document.getElementById('pre');
-            pre.textContent = xml.response;
-        }
+        xml.onload = function () { pre.textContent = xml.response; }
         let fr = new FileReader();
         fr.onload = function () {
             let dp = new DOMParser();
@@ -26,5 +37,9 @@ import * as DTE from './dtes'
         fr.readAsArrayBuffer(inp.files[0])
 
     })
+
 })()
+
+    
+
 
--- routes/sendmefile.ts
+++ routes/sendmefile.ts
@@ -1,7 +1,7 @@
-import * as express  from 'express';
+import * as express from 'express';
 import * as fs from 'fs';
 import * as DTE from '../cliente/dtes'
-import {DOMParser} from 'xmldom'
+import { DOMParser } from 'xmldom'
 
 
 export let router = express.Router();
@@ -10,21 +10,26 @@ router.post('/', function (req, res, next) {
     req.on('readable', function () {
         req.setEncoding('utf8')
         let str = <string>req.read();
-        let rd = new DOMParser({
-            locator: {},
-            errorHandler: {
-                warning: (w) => console.warn(w),
-                error: (e) => console.error(e),
-                fatalError: (e) => console.error(e)
-            }
-        })
-        let doc = rd.parseFromString(str, "application/xml")
-        let dd = new DTE.SetDTE();
-        dd.ParseFromXMLElement(doc.documentElement)
-        res.write(JSON.stringify(dd))
+        try {
+            let doc = new DOMParser({
+                locator: {},
+                errorHandler: {
+                    warning: (w) => console.warn('Advertencia: ' + w),
+                    error: (e) => console.error('Error: ' + e),
+                    fatalError: (e) => console.error('Error Fatal: ' + e)
+                }
+            }).parseFromString(str, "text/xml")
+
+        } catch (e) {
+            console.log(e)
+        }
+
+        // let dd = new DTE.SetDTE();
+        // dd.ParseFromXMLElement(doc.documentElement)
+        res.write(str)
         res.send();
     });
-        
+
 });
 
 router.get('/', (req, res) => {
