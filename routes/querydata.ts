d2eed3560bdc84be338ffcd7715074f5662abe5b
Diego Majuf <diego.majluf@gmail.com>
2016-10-17 09:04:05 -0200

    1

---
+++ routes/querydata.ts
@@ -0,0 +1,45 @@
+import * as express from 'express';
+import * as DTE from '../cliente/dtes';
+
+
+export let router = express.Router();
+
+router.get('/getventas/:tiempo/entre/:desde-:hasta', (req, res, next) => {
+
+
+})
+
+router.get('/getventasporcliente/:rut/:tiempo/entre/:desde-:hasta', (req, res, next) => {
+
+
+})
+
+router.get('/gettop/:num/clientes/:tiempo/entre/:desde-:hasta', (req, res, next) => {
+
+
+})
+
+router.get('/getventasporetiquetacliente/:nombre/:tiempo/entre/:desde-:hasta', (req, res, next) => {
+
+
+})
+
+router.get('/getventasporetiquetaitem/:nombre/:tiempo/entre/:desde-:hasta', (req, res, next) => {
+
+
+})
+
+router.get('/getventasporetiquetaitem/:nombre/:tiempo/entre/:desde-:hasta', (req, res, next) => {
+
+
+})
+
+router.get('/getventasporubicacion/:nombre/:tiempo/entre/:desde-:hasta', (req, res, next) => {
+
+
+})
+
+router.get('/getventasporubicacion/:nombre/:tiempo/entre/:desde-:hasta', (req, res, next) => {
+
+
+})
